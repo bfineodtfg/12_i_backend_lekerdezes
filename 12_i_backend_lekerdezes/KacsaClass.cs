19c386fea593bc3ca40dc61183bdf431c0a784d8
zsar.daniel <zsar.daniel@C19-TANAR>
2024-11-28 09:58:48 +0100

    szerda vége

---
+++ 12_i_backend_lekerdezes/KacsaClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_i_backend_lekerdezes
+{
+    public class KacsaClass
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int length { get; set; }
+    }
+}
--- 12_i_backend_lekerdezes/MainWindow.xaml.cs
+++ 12_i_backend_lekerdezes/MainWindow.xaml.cs
@@ -25,6 +25,65 @@ namespace _12_i_backend_lekerdezes
         public MainWindow()
         {
             InitializeComponent();
+            CreateTextBlock();
+        }
+
+        async void CreateTextBlock() {
+            kacsak.Children.Clear();
+            HttpClient client = new HttpClient();
+            string url = "http://127.0.0.1:3000/kacsa";
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                string stringResponse = await response.Content.ReadAsStringAsync();
+                List<KacsaClass> kacsaList = JsonConvert.DeserializeObject<List<KacsaClass>>(stringResponse);
+                foreach (KacsaClass item in kacsaList)
+                {
+                    Border oneBorder = new Border();
+                    kacsak.Children.Add(oneBorder);
+                    Grid oneGrid = new Grid();
+                    oneBorder.Child = oneGrid;
+                    RowDefinition firstRow = new RowDefinition();
+                    RowDefinition secondRow = new RowDefinition();
+                    RowDefinition thirdRow = new RowDefinition();
+                    oneGrid.RowDefinitions.Add(firstRow);
+                    oneGrid.RowDefinitions.Add(secondRow);
+                    oneGrid.RowDefinitions.Add(thirdRow);
+
+                    TextBlock NameTextBlock = new TextBlock();
+                    TextBlock LengthTextBlock = new TextBlock();
+                    Button SellButton = new Button();
+
+                    oneGrid.Children.Add(NameTextBlock);
+                    oneGrid.Children.Add(LengthTextBlock);
+                    oneGrid.Children.Add(SellButton);
+
+                    Grid.SetRow(LengthTextBlock, 1);
+                    Grid.SetRow(SellButton, 2);
+
+                    NameTextBlock.Text = $"Név: {item.name}";
+                    LengthTextBlock.Text = $"Hossz: {item.length}";
+                    SellButton.Content = "Eladás";
+
+                    oneBorder.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#484848"));
+                    oneBorder.Margin = new Thickness(5);
+                    oneBorder.CornerRadius = new CornerRadius(10);
+                    oneBorder.Padding = new Thickness(10);
+
+                    NameTextBlock.Foreground = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+                    LengthTextBlock.Foreground = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+
+                    //oneBlock.Text = $"Kacsa neve: {item.name}, kacsa hossza: {item.length}";
+
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+
+
         }
 
         async void AddKacsa(object s, EventArgs e) {
@@ -35,8 +94,8 @@ namespace _12_i_backend_lekerdezes
             {
                 var jsonObject = new
                 {
-                    name = nev.Text,
-                    length = hossz.Text
+                    name = KacsaNameTextBox.Text,
+                    length = KacsaLengthTextBox.Text
                 };
 
                 string jsonData = JsonConvert.SerializeObject(jsonObject);
@@ -44,14 +103,12 @@ namespace _12_i_backend_lekerdezes
 
                 HttpResponseMessage response = await client.PostAsync(url, data);
                 response.EnsureSuccessStatusCode();
+                CreateTextBlock();
             }
             catch (Exception error)
             {
                 MessageBox.Show(error.Message);
             }
-
-            
-
             //MessageBox.Show($"Kacsa neve: {nev.Text}, kacsa hossza: {hossz.Text}cm");
         }
     }
